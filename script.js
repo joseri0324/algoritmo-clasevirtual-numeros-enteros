ede05863a2c850a8ae8fc2e931b17d914973834c
jose <josesaa0324@gmail.com>
2024-11-05 21:23:35 -0500

    creacion de ciclo for y su cometarios

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+// Crear variables 
+let Negativos = 0;
+let Positivos = 0;
+let Mult15 = 0;
 
+// Inicio del ciclo for suministrando el índice
+for (let i = 1; i <= 5; i++) {
+    // Pregunta al usuario para que brinde la información
+    let num = parseInt(prompt("Ingresa el valor entero"+ " # "+" "+ i));
+    
+    // Verificación de cantidad negativa
+    if (num < 0) {
+        Negativos++;
+    } else if (num > 0) { // Verificación de cantidad positiva
+        Positivos++;
+    }
+    
+    // Verificación si es múltiplo de 15
+    if (num % 15 === 0) {
+        Mult15++;
+    }
+}
 
